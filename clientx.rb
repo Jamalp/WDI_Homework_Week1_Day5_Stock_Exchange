f9e3d912ff896e14befbe7d4c9f50c96442adaa1
Alina Guzman <alinochka8@gmail.com>
2013-06-21 17:39:29 -0400

    Added program files

---
+++ clientx.rb
@@ -0,0 +1,23 @@
+class Clientx
+  attr_accessor :name, :balance, :portfolio,
+
+  def initialize(name,balance)
+    @name = name
+    @balance = balance
+    @portfolio = {}
+  end
+
+  def buy_stock
+  end
+
+  def sell_stock
+  end
+
+  def list_clients
+  end
+
+  def list_portfolio
+  end
+
+end
+
