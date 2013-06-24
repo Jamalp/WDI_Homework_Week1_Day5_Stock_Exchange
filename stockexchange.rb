d508027c3158de7c67a9011da80e956dbabdd2ba
Alina Guzman <alinochka8@gmail.com>
2013-06-23 22:15:08 -0400

    Added remove stock and portfolio calculation option.

--- stockexchange.rb
+++ stockexchange.rb
@@ -1,19 +1,42 @@
-require_relative 'clientx.rb'
-require_relative 'portfolio.rb'
-require_relative 'stocks.rb'
+require_relative 'client.rb'
 require 'yahoofinance'
-
-alina = Clientx.new("Alina", 1000000)
-
+include ObjectSpace
 
 
-#alina.portfolio[:nyc] = google
+alina = Client.new("Alina", 10000000)
+##############################################NYC Portfolio
+alina.make_new_portfolio("NYC")
+goog = alina.buy_stock("GOOG", 10, "NYC")
+jnj = alina.buy_stock("JNJ", 10, "NYC")
+dis = alina.buy_stock("DIS", 10, "NYC")
 
-#puts alina.portfolio[:nyc]
+alina.sell_stock(goog, 10, "NYC")
+
+alina.sell_stock(jnj, 10, "NYC")
+###############################################Hong Kong Portfolio
+alina.make_new_portfolio("Hong Kong")
+alina.buy_stock("JNJ", 10, "Hong Kong")
+alina.buy_stock("C", 10, "Hong Kong")
+
+pavel = Client.new("Pavel", 10000000)
+
+kevin = Client.new("Kevin", 2000000)
+kevin.make_new_portfolio("Tech")
+kevin.buy_stock("BAC", 20, "Tech")
+
+alex = Client.new("Alex Guzman", 50000000)
+alex.make_new_portfolio("impossible")
+alex.buy_stock("GOOG", 20000, "impossible")
+alex.list_client_portfolios()
 
 
-#alina.buy_stock(stock_value_1)
-#alina.sell_stock(stock_value_1)
+eduard = Client.new("Eduard Guzman", 3000000)
+eduard.make_new_portfolio("Odessa")
+eduard.buy_stock("YHOO", 100, "Odessa")
+eduard.list_client_portfolios
 
-alina.buy_stock("GOOG", 1000)
-alina.sell_stock("GOOG", 500)
+
+alina.list_client_portfolios
+kevin.list_client_portfolios
+
+Client.list_all_clients
